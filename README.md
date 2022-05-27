e2ea38bc14ea940bc88d06962048e4625451224e
DanielGOnzalezz <castropdaniel13@hotmail.com>
2022-05-27 10:18:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-utm1j7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-utm1j7)
---
+++ index.ts
@@ -0,0 +1,23 @@
+import { calculaISV, Producto } from "./Deses-funci";
+
+
+
+
+
+
+const carritoCompras: Producto[] = [
+  {
+      desc: 'Telefono 1 ',
+      precio: 100
+  },
+  {
+      desc: 'Telefono 2',
+      precio: 150
+  },
+
+];
+
+const [total, isv] = calculaISV( carritoCompras);
+
+console.log('Total', total);
+console.log('ISV: ', isv);
